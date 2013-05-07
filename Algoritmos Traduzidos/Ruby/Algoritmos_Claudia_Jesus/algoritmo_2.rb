564a7c9ae112f7259035ca0d9d2fcc4a942ad6a4
bgpinheiro <Bruno.Pinheiro82@gmail.com>
2013-05-08 00:00:38 +0100

    Mais algoritmos em Ruby

---
+++ Algoritmos Traduzidos/Ruby/Algoritmos_Claudia_Jesus/algoritmo_2.rb
@@ -0,0 +1,10 @@
+#Entrar com dois números e imprimir o número menor (suponha números diferentes)
+puts "digite o primeiro numero"
+a = gets
+puts "digite o segundo numero"
+b = gets
+if a.to_i < b.to_i
+  puts "Menor; " + a
+else
+  puts "Menor: " + b
+end
