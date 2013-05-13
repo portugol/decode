1c783717f5dbd8f08718a2c3b4c494b2af74309f
bgpinheiro <Bruno.Pinheiro82@gmail.com>
2013-05-14 00:06:24 +0100

    Equivalências Ruby

---
+++ Apresentação 3/Ruby/algoritmo_005.rb
@@ -0,0 +1,12 @@
+def factorial(k)
+  if k > 2
+    j = k * factorial(k - 1)
+  else
+    j = k
+  end
+end
+
+puts "Introduza o número do factorial a calcular"
+i = gets
+print "Factorial de #{i.to_i} é: " + factorial(i.to_i).to_s
+
