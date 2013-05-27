dd4b49d17f247305f4785128ace5a66803c53d6a
eliomachado <eliomachado@gmail.com>
2013-05-27 16:23:11 +0100

    Update!

--- Algoritmos Traduzidos/Pascal/PSI[Tiago]- Tradução.pas
+++ Algoritmos Traduzidos/Pascal/PSI[Tiago]- Tradução.pas
@@ -14,6 +14,7 @@ Var
 	write('A area é: ' , area);
  End.
  
+ ----------------------------------------------------------------
  
 Alg_Área do quadrado
 Elabore um algoritmo que peça o valor de um lado e imprima a área do quadrado.
@@ -28,7 +29,8 @@ Elabore um algoritmo que peça o valor de um lado e imprima a área do quadrado.
  	write('area = ' , area);
  End.
  
- 
+  ----------------------------------------------------------------
+  
 Alg_Área do rectângulo
 Elabore um algoritmo que peça o valor da altura e da largura e imprima a área do rectângulo.
  
@@ -44,6 +46,8 @@ Elabore um algoritmo que peça o valor da altura e da largura e imprima a área 
  	write('area = ', area);
  End.
  
+  ----------------------------------------------------------------
+  
  Alg_Perimetro do quadrado
 Elabore um algoritmo que peça o valor de um lado e imprima o perimetro do quadrado.
  
@@ -55,3 +59,56 @@ Elabore um algoritmo que peça o valor de um lado e imprima o perimetro do quadr
  	perimetro := lado * 4;
  	write('perimetro = ', perimetro);
  End.
+ 
+----------------------------------------------------------------
+Alg_Perimetro do rectângulo
+Elabore um algoritmo que peça o valor da altura e da largura e imprima o perimetro do rectângulo.
+
+var base, altura, perimetro: Real;
+ Begin
+ 	base := 0.0;
+	altura := 0.0;
+	write('base: ');
+	readln(base);
+	write('altura: ');
+	readln(altura);
+	perimetro := 2 * (base + altura);
+	write('perimetro = ' , perimetro);
+ End.
+----------------------------------------------------------------
+Alg_Potência
+Elabore um algoritmo que peça o valor da base e o valor do expoente e imprima a sua potência.
+
+var 
+	b, p : Integer;
+	res : real;
+ Begin
+ 	b := 0;
+ 	p := 0;
+ 	res := 0;
+	write('introduza a base: ');
+	readln(b);
+	write('introduza a potencia: ');
+	readln(p);
+	res := exp(ln(b)*p);
+	write('o resultado da potencia é: ' , res);
+ End.
+ 
+----------------------------------------------------------------
+Alg_Volume do Paralelepípedo
+Elabore um algoritmo que peça o valor da altura, do lado maior e menor e imprima o volume do rectângulo.
+
+var lmaior, lmenor, altura, volume : Real;
+ Begin
+ 	lmaior := 0.0;
+ 	lmenor := 0.0;
+ 	altura:= 0.0;
+	write('introduza lado maior  lado menor e a altura por esta ordem ');
+	readln(lmaior);
+	readln(lmenor);
+	readln(altura);
+	volume := lmaior*lmenor*altura;
+	write('o volume é :', volume);
+ End.
+ 
+----------------------------------------------------------------
