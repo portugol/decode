51ef20433dc3555794ec811d9a2a875520092eaa
Ttome <tiago.mdt@gmail.com>
2013-06-26 16:37:25 +0100

    completo

---
+++ Apresentação 3/Python/do_while.py
@@ -0,0 +1,6 @@
+i= 0
+while True:
+    i=input('Digite um número:')
+    int(i)<0
+    if int(i)>=0:
+        break
---
+++ Apresentação 3/Python/funcoes_final.py
@@ -0,0 +1,18 @@
+
+
+def fact(k):
+    
+    if k >2 :
+        return (k * fact(k - 1) )
+    else:
+        return k
+    
+i=5
+
+
+j=fact(i)
+
+print (j)
+
+
+
