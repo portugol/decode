1b39e5142efd53d40a2b8f1e5249066031940091
bgpinheiro <Bruno.Pinheiro82@gmail.com>
2013-07-02 01:16:22 +0100

    Traduções em falta

---
+++ Algoritmos Traduzidos/Perl/Algoritmos_Tiago/Alg_Area_Quadrado.pl
@@ -0,0 +1,5 @@
+#Elabore um algoritmo que peça o valor de um lado e imprima a área do quadrado.
+print "Introduza o valor do lado do quadrado:\n";
+$lado = <STDIN>;
+$area = $lado * $lado;
+print "\narea: $area\n";
---
+++ Algoritmos Traduzidos/Perl/Algoritmos_Tiago/Alg_Potencia.pl
@@ -0,0 +1,7 @@
+#Elabore um algoritmo que peça o valor da base e o valor do expoente e imprima a sua potência.
+print "base:\n";
+$base = <STDIN>;
+print "potencia:\n";
+$potencia = <STDIN>;
+$res = $base ** $potencia;
+print "O resultado da potencia e: $res\n";
