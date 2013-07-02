e9fb1bf07895ee8847f255e6bede94ecda90209f
eliomachado <eliomachado@gmail.com>
2013-07-02 12:09:29 +0100

    Update!

--- Algoritmos Traduzidos/Pascal/Algoritmos organizados/Algoritmos organizados/repeticao - tradução.pas
+++ Algoritmos Traduzidos/Pascal/Algoritmos organizados/Algoritmos organizados/repeticao - tradução.pas
@@ -3,8 +3,29 @@ REPETIÇÃO
 
 Escrever um número de 1 a 10.
 
+Var i: Integer;
+ Begin
+ 	i := 1;
+	While (i <= 10) Do
+	Begin
+		writeln(i);
+		i := i + 1;
+	End;
+ End.
+
+
 
 Pedir um número positivo.
+
+Var i : Integer;
+ Begin
+ 	i := 0;
+	repeat
+	Begin
+		readln(i);
+	End;
+	until(i < 0 )
+ End.
 
 
 Filipe Farinha
@@ -32,40 +53,20 @@ var li, ls, s : Integer;
 2
 Implemente um algoritmo que permite a introdução do número do mês do nascimento. O algoritmo só deve terminar quando o mês for válido (entre 1 e 12).
 
-
-3
-Imagine que um utilizador solicita a execução do algoritmo “estação climática” e digita um número respeitante à estação escolhida. O algoritmo só deve continuar quando a opção escolhida for válida (entre 1 e 5).
-
-var opcao : Integer;
-Begin
-     writeln('1 - Verão');
-     writeln('2 - Outono');
-     writeln('3 - Inverno');
-     writeln('4 - Primavera');
-     while (opcao < 1) OR (opcao > 4) DO
-     Begin
-          writeln('Escolha a op‡Æo do menu referente ao per¡odo desejado:');
-          readln(opcao);
+var mes : Integer;
+ Begin
+ 	mes := 0;
+	repeat
+	Begin 
+		write('Introduza um mes: ') ; 
+ 		read(mes);
+     	writeln('mes introduzido: ', mes) ; 
      End;
-     Case opcao of
-     1 :
-       Begin
-            writeln('Verão');
-       End;
-     2 :
-       Begin
-            writeln('Outono');
-       End;
-     3 :
-       Begin
-            writeln('Inverno');
-       End;
-     4 :
-       Begin
-            writeln('Primavera');
-       End;
-   End;
-End.
+ 	until (mes < 0) or (mes > 13);
+ 	
+ End.
+
+
 
 4
 Faça um algoritmo que imprima todos os números impares no intervalo de 1 a 100.
@@ -105,35 +106,7 @@ var p, num1, num : Integer;
 	End;
  End.
 
-7
-Elabore um algoritmo que simule uma contagem regressiva de 10 minutos, ou seja, mostre 10:00, 9:59, 9:58, ..., 9:00; 8:59, 8:58... até 0:00.
 
-Este alguritmo leva FOR;
-
-
-8
-Desenvolva um algoritmo que calcule o valor de n a partir da seguinte expressão matemática: (inserir imagem imagem_001).
-
-var 
-	k,n : Integer;
-	pi : real;
- Begin
-	write('Digite o valor de n: ');
-	read(n);
-	for  k := 0 to n do
-	Begin
-		if(k mod 2 = 0)then
-		Begin
-			pi := pi + (1 / (2 * k + 1));
-		End
-		else
-		Begin
-			pi := pi - (1 / (2 * k + 1));
-		End;
-	End;
-	pi := 4 * pi;
-	write('O valor de pi é: ' , pi);  
- End.
 
 11
 Dado um dado país A com 5.000.000 de habitantes e uma taxa de natalidade de 3% ao ano, e um país B com 7.000.000 de habitantes e uma taxa de natalidade de 2% ao ano, calcular e imprimir o tempo necessário para que a população do país A ultrapasse a população do país B.
