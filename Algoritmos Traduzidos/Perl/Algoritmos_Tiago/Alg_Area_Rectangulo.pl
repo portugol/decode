0d3ee14fc7dc8fb8f961aeeadce3a10a4acbb2d7
bgpinheiro <Bruno.Pinheiro82@gmail.com>
2013-07-02 01:02:16 +0100

    Traduções em falta

---
+++ Algoritmos Traduzidos/Perl/Algoritmos_Tiago/Alg_Area_Rectangulo.pl
@@ -0,0 +1,7 @@
+#Elabore um algoritmo que peça o valor da altura e da largura e imprima a área do rectângulo
+print "base:\n";
+$base = <STDIN>;
+print "altura:\n";
+$altura = <STDIN>;
+$area = $base * $altura;
+print "\narea: $area\n";
---
+++ Algoritmos Traduzidos/Perl/Algoritmos_Tiago/Alg_Perimetro_Quadrado.pl
@@ -0,0 +1,5 @@
+#Elabore um algoritmo que peça o valor de um lado e imprima o perimetro do quadrado.
+print "Introduza o valor do lado do quadrado:\n";
+$lado = <STDIN>;
+$perimetro = $lado * 4;
+print "\nperimetro: $perimetro\n";
